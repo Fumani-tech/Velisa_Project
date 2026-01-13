3a1367535985e934c1036fdf49bffd371d2103b1
Neo Sathikge <neossadiki@gmail.com>
2026-01-13 15:11:08 +0200

    Added half.py and average.py

---
+++ funtions/average.py
@@ -0,0 +1,7 @@
+# Write a function `average` that accepts three numbers as arguments.
+# The function should return the average of the three numbers.
+
+
+print(average(3, 10, 8))    # 7
+print(average(10, 5, 12))   # 9
+print(average(6, 20, 40))   # 22
