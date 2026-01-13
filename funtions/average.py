d9afdea2946b9607a63758de02ad60194e8281e8
Neo Sathikge <neossadiki@gmail.com>
2026-01-13 15:34:57 +0200

    updated the average file

--- funtions/average.py
+++ funtions/average.py
@@ -2,6 +2,9 @@
 # The function should return the average of the three numbers.
 
 
+def average(a, b, c):
+    return (a + b + c) / 3
+
 print(average(3, 10, 8))    # 7
 print(average(10, 5, 12))   # 9
-print(average(6, 20, 40))   # 22
+print(average(6, 20, 40))   # 22        
