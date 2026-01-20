304ff91e91e6c629c769308c1e227e1e8e004062
Neo Sathikge <neossadiki@gmail.com>
2026-01-20 14:45:33 +0200

    Completed min_to_max.py exercise

---
+++ Loops/A_loops_exercise/min_to_max.py
@@ -0,0 +1,10 @@
+# Write a function `min_to_max(min_num, max_num)` that prints all numbers from min to max inclusive.
+
+def min_to_max(min_num,max_num):
+	for i in range(min_num,max_num+1):
+		print(i)
+
+
+min_to_max(5, 9)
+min_to_max(11, 13)
+min_to_max(20, 11)  # nothing   # what happens here?
