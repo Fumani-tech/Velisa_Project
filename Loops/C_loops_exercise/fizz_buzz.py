c6f3d2764bf93486493c070aa0bd28c41fee45df
Neo Sathikge <neossadiki@gmail.com>
2026-01-21 17:56:05 +0200

    Completed fizz_buzz.py exercise

---
+++ Loops/C_loops_exercise/fizz_buzz.py
@@ -0,0 +1,21 @@
+# Write a function `fizz_buzz(max_num)` that prints all numbers <= max_num
+# that are divisible by 3 or 5 but not both.
+# The function does not return a value, just prints.
+
+# Example:
+
+
+def fizz_buzz(max_num):
+	for i in range(max_num+1):
+		if (i % 3 == 0 or i % 5 == 0) and not (i % 3 == 0 and i % 5 == 0):
+			print (i)
+			
+
+fizz_buzz(18)
+# 3
+# 5
+# 6
+# 9
+# 10
+# 12
+# 18
