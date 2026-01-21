84c45b909377febb8a8b0cf166a88186c369cec4
Neo Sathikge <neossadiki@gmail.com>
2026-01-21 15:15:29 +0200

    Completed  reverse_iterate.py exercise

---
+++ Loops/C_loops_exercise/reverse_iterate.py
@@ -0,0 +1,22 @@
+# Write a function `reverse_iterate(text)` that prints each character of the string
+# in reverse order. The function does not return a value, just prints.
+
+# Example:
+
+def reverse_iterate(text):
+	for i in range(len(text)-1,-1,-1):
+		print (text[i])
+
+
+reverse_iterate("carrot")
+# t
+# o
+# r
+# r
+# a
+# c
+
+reverse_iterate("box")
+# x
+# o
+# b
