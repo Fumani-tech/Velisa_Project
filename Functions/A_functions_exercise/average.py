63e55455d738dc37077fe9aa196e0c3971e0b3ab
Neo Sathikge <neossadiki@gmail.com>
2026-01-24 22:41:50 +0200

    Updated average.py

---
+++ Functions/A_functions_exercise/average.py
@@ -0,0 +1,10 @@
+# Write a function `average` that accepts three numbers as arguments.
+# The function should return the average of the three numbers.
+
+
+def average(a, b, c):
+    return (a + b + c) / 3
+
+print(average(3, 10, 8))    # 7
+print(average(10, 5, 12))   # 9
+print(average(6, 20, 40))   # 22        
