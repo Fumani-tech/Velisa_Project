5b82738bad0957018ec6e8ef54f5d86a45328ec7
Neo Sathikge <neossadiki@gmail.com>
2026-01-24 23:43:18 +0200

    Modified parity.py

---
+++ Functions/B_functions_exercise/parity.py
@@ -0,0 +1,18 @@
+# Write a function `parity` that accepts a number.
+# Return the string "even" if the number is even.
+# Return the string "odd" if the number is odd.
+
+def parity(num):
+	if num % 2 == 0:
+		return "even"
+	else:
+		return "odd"
+	
+
+
+print(parity(5))       # "odd"
+print(parity(7))       # "odd"
+print(parity(13))      # "odd"
+print(parity(32))      # "even"
+print(parity(10))      # "even"
+print(parity(602348))  # "even"
