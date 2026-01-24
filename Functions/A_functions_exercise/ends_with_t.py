88db4b7cb0e3bed88defb8667d966b92f3f05df7
Neo Sathikge <neossadiki@gmail.com>
2026-01-24 22:44:33 +0200

    Updated ends_with_t.py

---
+++ Functions/A_functions_exercise/ends_with_t.py
@@ -0,0 +1,15 @@
+# Write a function `ends_with_t` that accepts a string as an argument.
+# The function should return a boolean indicating whether the string
+# ends with the character "t".
+def ends_with_t(s):
+    if s.endswith("t"):
+        return True
+    else:
+        return False
+
+print(ends_with_t("smart"))      # True
+print(ends_with_t("racket"))     # True
+print(ends_with_t("taco"))       # False
+print(ends_with_t("boomerang"))  # False
+
+
