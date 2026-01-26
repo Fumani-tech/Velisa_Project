eeff37e532fe841efb4e713b82bdef64fff3ab1a
Neo Sathikge <neossadiki@gmail.com>
2026-01-26 16:19:43 +0200

    Updated case_changer.py

---
+++ Functions/C_functions_exercise/case_changer.py
@@ -0,0 +1,14 @@
+#Write a function case_change(text, make_upper):
+#if make_upper is True return the string in uppercase
+#if False return it in lowercase
+
+def case_change(text,make_upper):
+	if (make_upper == True):
+		return text.upper()
+	else:
+		return text.lower()
+
+print(case_change("Super", True))      # SUPER
+print(case_change("Super", False))     # super
+print(case_change("tAmBourine", True)) # TAMBOURINE
+print(case_change("tAmBourine", False))# tambourine
