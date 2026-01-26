b65d8a032ea2c6a28680a0503961feb449aaed13
Neo Sathikge <neossadiki@gmail.com>
2026-01-26 18:32:49 +0200

    Updated is_palindrome.py

---
+++ Functions/C_functions_exercise/is_palindrome.py
@@ -0,0 +1,10 @@
+#Write a function that checks if a string is a **palindrome** (same forward & backward).
+
+#Ignore spaces and capitalization.
+def is_palindrome(text):
+	text2 = text.replace(" ", "")
+	return text == text2[::-1]
+
+print(is_palindrome("level"))          # True
+print(is_palindrome("Race car"))       # True
+print(is_palindrome("python"))         # False
