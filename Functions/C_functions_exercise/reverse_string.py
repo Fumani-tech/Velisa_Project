4b8592829b7509a9a32f38050c974ddf1f35503a
Neo Sathikge <neossadiki@gmail.com>
2026-01-26 18:12:26 +0200

    Updated reverse_string.py

---
+++ Functions/C_functions_exercise/reverse_string.py
@@ -0,0 +1,6 @@
+#Write a function reverse_string(text) that returns the reversed string.
+
+def reverse_string(s):
+	return s[::-1]
+print(reverse_string("hello"))   # olleh
+print(reverse_string("Python"))  # nohtyP
