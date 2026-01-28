8e4bc4f21d4313fbe5eb06d609059c235ae20740
Neo Sathikge <neossadiki@gmail.com>
2026-01-28 15:44:58 +0200

    Updated one.py exercise

---
+++ conditional/A_conditional_exercise/one.py
@@ -0,0 +1,40 @@
+# snippet 1 - 0
+qty = 38
+
+if qty > 30 and qty % 5 == 4:
+    print("swish")
+else:
+    print("swoosh")
+
+if qty > 0:
+    print("pos")
+
+# snippet 1 - 1
+a = "celery"
+b = "SQUASH"
+
+if a == a.upper():
+    print("alpha")
+
+if b == b.upper():
+    print("beta")
+
+# snippet 1 - 2
+number = 9
+
+if number > 4:
+    print("ding")
+elif number % 3 == 0:
+    print("dong")
+
+# snippet 1 - 3
+z = 12
+
+if z > 10:
+    print("vroom")
+
+if z % 3 == 0:
+    print("skrrt")
+
+
+
