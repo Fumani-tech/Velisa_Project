76a280c2564f922b2ef1de90bf6065d82ee403d2
Neo Sathikge <neossadiki@gmail.com>
2026-01-28 15:25:01 +0200

    Completed make_matrix.py exercise

---
+++ Nesting/B_nesting_exercise/make_matrix.py
@@ -0,0 +1,15 @@
+# Write a function `make_matrix(m, n, value)` that returns a 2D list of m rows and n columns
+# filled with `value`.
+
+def make_matrix(m, n, value):
+    matrix = []
+    for i in range(m):
+        row = []
+        for j in range(n):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+print(make_matrix(3, 5, None))
+print(make_matrix(4, 2, "x"))
+print(make_matrix(2, 2, 0))
+print(make_matrix(6, 3, "2"))
