de55e785cdfd38e4bf8b391124e3c8dca96ba853
Neo Sathikge <neossadiki@gmail.com>
2026-01-28 15:52:11 +0200

    Updated extra.py exercise

---
+++ conditional/A_conditional_exercise/extra.py
@@ -0,0 +1,52 @@
+#Extra 1 — Odd or Even
+num = 17
+
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+#Extra 2 — Password Strength
+password = "Code123"
+
+if len(password) < 4:
+    print("too short")
+elif password.isalpha():
+    print("weak")
+elif password.isalnum():
+    print("medium")
+else:
+    print("strong")
+
+#Extra 3 — Temperature Detector
+temp = 32
+
+if temp < 0:
+    print("freezing")
+elif temp < 20:
+    print("cold")
+elif temp < 30:
+    print("warm")
+else:
+    print("hot")
+
+#Extra 4 — Vowel or Consonant
+ch = "e"
+
+if ch.lower() in "aeiou":
+    print("vowel")
+else:
+    print("consonant")
+
+#Extra 5 — Age Group Classifier
+age = 14
+
+if age < 13:
+    print("child")
+elif age < 20:
+    print("teen")
+elif age < 60:
+    print("adult")
+else:
+    print("senior")
+
