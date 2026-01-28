0cd99a2edc0f82d54c5fb83ab476a1139314f53c
Neo Sathikge <neossadiki@gmail.com>
2026-01-28 14:46:38 +0200

    Completed print_2d.py exercise

---
+++ Nesting/B_nesting_exercise/print_2d.py
@@ -0,0 +1,43 @@
+#B_nesting_loops
+# Write a function `print2d(matrix)` that accepts a 2D list and prints all inner elements.
+
+def print2d(array1):
+    for row in array1:
+        for item in row:
+            print(item)
+
+array1 = [
+    ["a", "b", "c", "d"],
+    ["e", "f"],
+    ["g", "h", "i"]
+]
+
+print2d(array1)
+# prints:
+# a
+# b
+# c
+# d
+# e
+# f
+# g
+# h
+# i
+
+def print2d(array2):
+    for row in array2:
+        for item in row:
+            print(item)
+
+array2 = [[9, 3, 4],
+            [11],
+            [42, 100]]
+print2d(array2)
+# prints:
+# 9
+# 3
+# 4
+# 11
+# 42
+# 100
+
