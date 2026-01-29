7ed6d19db887a4d9c6aadeaba49201af6e99b56e
Neo Sathikge <neossadiki@gmail.com>
2026-01-29 14:17:42 +0200

    Completed zipper.py exercise

---
+++ Nesting/B_nesting_exercise/zipper.py
@@ -0,0 +1,13 @@
+# Write a function `zipper(list1, list2)` that returns a 2D list containing pairs of elements at
+# the same indices. Assume lists have same length.
+
+def zipper(list1, list2):
+    return [[list1[i], list2[i]] for i in range(len(list1))]
+
+array1 = ["a", "b", "c", "d"]
+array2 = [-1, -2, -3, -4]
+print(zipper(array1, array2))
+
+array3 = ["whisper", "talk", "shout"]
+array4 = ["quiet", "normal", "loud"]
+print(zipper(array3, array4))
