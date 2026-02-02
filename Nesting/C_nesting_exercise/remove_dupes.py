7c207487843ccea035b723a72a6879921c161caa
Neo Sathikge <neossadiki@gmail.com>
2026-02-02 14:12:34 +0200

    Updated shorten_long_words.py exercise

--- Nesting/C_nesting_exercise/remove_dupes.py
+++ Nesting/C_nesting_exercise/remove_dupes.py
@@ -12,3 +12,10 @@ print(remove_dupes(["x", "y", "y", "x", "z"]))  # ['x', 'y', 'z']
 print(remove_dupes([False, False, True, False]))  # [False, True]
 print(remove_dupes([42, 5, 7, 42, 7, 3, 7, 7]))  # [42, 5, 7, 3]
 
+
+# def remove_dupes(lst):
+#     new_list = []
+#     for item in lst:
+#         if item not in new_list:
+#             new_list.append(item)
+#     return new_list
