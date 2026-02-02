068139a90d5b1d13633edf050721f5bc17177096
Neo Sathikge <neossadiki@gmail.com>
2026-02-02 14:13:42 +0200

    Updated remove_first_vowel.py exercise

--- Nesting/C_nesting_exercise/remove_first_vowel.py
+++ Nesting/C_nesting_exercise/remove_first_vowel.py
@@ -1,19 +1,13 @@
 # Write a function `remove_first_vowel(s)` that accepts a string and returns the string
 # with its first vowel removed.
 
-def remove_first_vowel(s):
-    new_str = ''
-    vowel_found = False
-    for i in range(len(s)):
-        if s[i] not in 'aeiou'  :
-            new_str+= s[i]
-        else:
-            vowel_found = True
-            break
-    if vowel_found:
-        new_str += s[i+1:]
-    return new_str
 
+def remove_first_vowel(s):
+    for i in range(len(s)):
+        if s[i] in 'aeiou':
+            first = s[:i]
+            last = s[i+1:]
+            return first + last
 
 # Example usage:
 print(remove_first_vowel("volcano"))  # 'vlcano'
